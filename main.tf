9a8d17f1a7880eef761d288e910970606ccb0f73
SKashyap001 <143029753+SKashyap001@users.noreply.github.com>
2025-06-26 14:26:18 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,9 +1,9 @@
 provider "aws" {
-  region = "ap-south-1" # Change region as needed
+  region = "ap-south-1" 
 }
 
 resource "aws_s3_bucket" "my_bucket" {
-  bucket = "my-unique-bucket-name-12345" # Must be globally unique
+  bucket = "my-unique-bucket-name-12345sunny" 
 
   tags = {
     Name        = "sunnybucket"
